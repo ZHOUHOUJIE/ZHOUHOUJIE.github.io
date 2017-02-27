543cec3c91088edfa71d101d1fc7a22c5cc076f7
ZHOUfree <541526968@qq.com>
2017-02-27 17:45:57 +0800

    demo0.1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var mySwiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        direction: 'vertical'
+    })
+})
