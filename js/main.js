2675b4c010e07a4f2971c752358f1ab1325732bc
ZHOUfree <541526968@qq.com>
2017-02-28 21:54:19 +0800

    Ver1.0

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
         pagination: '.swiper-pagination',
         paginationClickable: true,
         direction: 'vertical',
-        mousewheelControl: true
+        mousewheelControl: true,
+        onInit: function(swiper) { //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素 
+            swiperAnimate(swiper); //初始化完成开始动画
+        },
+        onSlideChangeEnd: function(swiper) {
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        }
     })
 })
